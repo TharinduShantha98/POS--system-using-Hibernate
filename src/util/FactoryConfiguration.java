f3772aeb91ca8894b843664127879f1bd1b1e26a
TharinduShantha98 <tharindushantha4@gmail.com>
2021-11-25 10:31:05 -0800

    created hibernate.cfg.xml and FactroyConfiguration

---
+++ src/util/FactoryConfiguration.java
@@ -0,0 +1,29 @@
+package util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+public class FactoryConfiguration {
+
+    private  static  FactoryConfiguration factoryConfiguration = null;
+    private  SessionFactory sessionFactory;
+    private FactoryConfiguration(){
+        Configuration configuration = new Configuration().configure();
+        sessionFactory = configuration.buildSessionFactory();
+
+
+    }
+
+    public  static FactoryConfiguration getInstance(){
+        return (factoryConfiguration == null) ? factoryConfiguration = new FactoryConfiguration():factoryConfiguration;
+
+    }
+
+    public Session getSession(){
+        return  sessionFactory.openSession();
+    }
+
+
+
+
+}
